5dc30c96bd3e9c29e74e2a3ce06cdc83f3be252a
Lantip <lantip@192.168.1.8>
2022-02-07 13:41:09 +0700

    prettier check

--- src/lib/share.ts
+++ src/lib/share.ts
@@ -5,8 +5,8 @@ import { GAME_TITLE } from '../constants/strings'
 export const shareStatus = (guesses: string[], lost: boolean) => {
   navigator.clipboard.writeText(
     `${GAME_TITLE} ${solutionIndex} ${lost ? 'X' : guesses.length}/6\n\n` +
-    generateEmojiGrid(guesses) +
-    '\n\nhttps://batangan.lantip.xyz'
+      generateEmojiGrid(guesses) +
+      '\n\nhttps://batangan.lantip.xyz'
   )
 }
 
