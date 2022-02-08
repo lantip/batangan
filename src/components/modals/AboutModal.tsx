28de1966c4f6a8980d4b59de3fc71a6c389b21a7
Lantip <lantip@192.168.1.4>
2022-02-09 06:30:39 +0700

    popup hasil permainan dalam bahasa jawa #5

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -7,27 +7,27 @@ type Props = {
 
 export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
-    <BaseModal title="About" isOpen={isOpen} handleClose={handleClose}>
+    <BaseModal title="Tentang" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        This is an open source word guessing game -{' '}
+        Punika dolanan bedhèk-bedhèkan <i>open source</i> (sumber tinarbuka) -{' '}
         <a
           href="https://github.com/lantip/batangan"
           className="underline font-bold"
           target="_blank"
           rel="noreferrer"
         >
-          check out the code here
+          kode sumber dolanan wonten mriki
         </a>{' '}
       </p>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Its forked from the original wordle game -{' '}
+        Dolanan punika mundhut sumber kode saking wordle game -{' '}
         <a
           href="https://github.com/bhavesh15184/word-guessing-game"
           className="underline font-bold"
           target="_blank"
           rel="noreferrer"
         >
-          check out the original code here
+          kode asalipun wonten mriki
         </a>{' '}
       </p>
     </BaseModal>
--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -1,19 +1,19 @@
 export const GAME_TITLE = 'Batangan'
 
 export const WIN_MESSAGES = ['Wangun!', 'Njenengan Ngeten 👍!', 'Sangar!']
-export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
+export const GAME_COPIED_MESSAGE = 'Skor terkopi. Mangga ctrl+v teng medsos.'
 export const ABOUT_GAME_MESSAGE = 'Tentang game ini'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'Aksara kirang jangkep!'
-export const WORD_NOT_FOUND_MESSAGE = 'Mboten wonten ukara punika'
+export const WORD_NOT_FOUND_MESSAGE = 'Ukara mboten kepanggih wonten kamus'
 export const CORRECT_WORD_MESSAGE = (solution: string, makna: string) =>
   `Batangane yaiku ${solution} → ${makna}`
 export const ENTER_TEXT = 'Enter'
 export const DELETE_TEXT = 'Delete'
-export const STATISTICS_TITLE = 'Statistics'
-export const GUESS_DISTRIBUTION_TEXT = 'Guess Distribution'
-export const NEW_WORD_TEXT = 'New word in'
+export const STATISTICS_TITLE = 'Statistik'
+export const GUESS_DISTRIBUTION_TEXT = 'Sebaran Batangan'
+export const NEW_WORD_TEXT = 'Ukara anyar ing'
 export const SHARE_TEXT = 'Share'
-export const TOTAL_TRIES_TEXT = 'Total tries'
-export const SUCCESS_RATE_TEXT = 'Success rate'
-export const CURRENT_STREAK_TEXT = 'Current streak'
-export const BEST_STREAK_TEXT = 'Best streak'
+export const TOTAL_TRIES_TEXT = 'Nyoba ping'
+export const SUCCESS_RATE_TEXT = 'Kasil'
+export const CURRENT_STREAK_TEXT = 'mBatang Ping'
+export const BEST_STREAK_TEXT = 'Batang apik'
